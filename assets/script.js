7e9d44d9c6c2a01ba5cf15f8890673b0282280e6
Karlyweatherstein <karlyweatherstein@gmail.com>
2022-02-27 00:15:28 -0500

    added CSS styling

--- assets/script.js
+++ assets/script.js
@@ -14,12 +14,102 @@ function startTimer(){
     }, 1000);
   }
 function countdown() {
-    document.getElementById("start").addEventListener("click", startTimer);
-    
     startTimer();
 };
-
 
 document.getElementById("start").addEventListener("click", startTimer);
 
 // Timer End
+
+// Game start
+
+var highScore = ""
+var startButton = document.getElementById("start")
+var nextButton = document.getElementById("next-btn")
+var hideExtraInfo = document.getElementById("hide-this")
+var questionConEl = document.getElementById("question-container")
+var questionEl = document.getElementById("question")
+var answerButtonsEl = document.getElementById("answer-button")
+var shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener("click", startGame)
+nextButton.addEventListener("click", () => {
+    currentQuestionIndex++
+    nextQuestion()
+})
+
+
+function startGame() {
+    startButton.classList.add("hide")
+    hideExtraInfo.classList.add("hide")
+    shuffledQuestions = questions.sort(() => Math.random())
+    currentQuestionIndex = 0
+    questionConEl.classList.remove("hide")
+    nextQuestion()
+}
+
+function nextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionEl.innerText = question.question
+    question.answers.forEach(answer => {
+        var button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerButtonsEl.appendChild(button)
+    })
+}
+
+function resetState() {
+    nextButton.classList.add("hide")
+    while (answerButtonsEl.firstChild) {
+        answerButtonsEl.removeChild(answerButtonsEl.firstChild)
+    }
+}
+
+function selectAnswer(e) {
+    var selectBtn = e.target
+    var correct = selectBtn.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsEl.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length)
+    nextButton.classList.remove("hide")
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element) 
+    if (correct) {
+        element.classList.add("correct")
+    } else {
+        element.classList.add("wrong")
+    }
+    
+}
+
+function clearStatusClass(element) {
+    element.classList.remove("correct")
+    element.classList.remove("wrong")
+
+}
+
+var questions = [
+    {
+        question: "Commonly used data types DO not include:",
+        answers: [
+            {text: "1. Strings", correct: false},
+            {text: "2. Booleans", correct: false},
+            {text: "3. Alerts", correct: true},
+            {text: "4. Numbers", correct: false},
+
+        ]
+    }
+]
